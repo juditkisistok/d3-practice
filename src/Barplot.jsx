136c4956d339fccc7a357ce71357e9062bd518da
Judit Kisistok <judit.kisistok@gmail.com>
2026-03-20 22:53:54 +0100

    add numbers to bars

--- src/Barplot.jsx
+++ src/Barplot.jsx
@@ -1,7 +1,8 @@
 import AxisBottom from "./assets/AxisBottom";
-import AxisLeft from "./AxisLeft";
+import { useState } from "react";
 
 export default function Barplot({ data, width, height, axisY, axisX, margin }) {
+  const [hovered, setHovered] = useState(null);
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left}, ${margin.top})`}>
@@ -10,18 +11,50 @@ export default function Barplot({ data, width, height, axisY, axisX, margin }) {
           innerWidth={width - margin.left - margin.right}
           innerHeight={height - margin.top - margin.bottom}
         />
-        <AxisLeft axisY={axisY} />
+
         {data.map((d) => (
-          <rect
+          <g
             key={d.country}
-            x={0}
-            y={axisY(d.country)}
-            width={axisX(d.students)}
-            height={axisY.bandwidth()}
-            fill="#96929c"
-            rx={3}
-          />
+            onMouseEnter={() => setHovered(d.country)}
+            onMouseLeave={() => setHovered(null)}
+          >
+            <text
+              x={-12}
+              y={axisY(d.country) + axisY.bandwidth() / 2}
+              textAnchor="end"
+              dominantBaseline="middle"
+            >
+              {d.country}
+            </text>
+            <rect
+              key={d.country}
+              x={0}
+              y={axisY(d.country)}
+              width={axisX(d.students)}
+              height={axisY.bandwidth()}
+              fill="#96929c"
+              rx={3}
+            />
+          </g>
         ))}
+        {hovered &&
+          (() => {
+            const d = data.find((d) => d.country === hovered);
+            const x = axisX(d.students);
+            const y = axisY(hovered) + axisY.bandwidth() / 2;
+            return (
+              <g>
+                <path
+                  d={`M ${x + 4} ${y} C ${x + 15} ${y - 12}, ${x + 15} ${y + 12}, ${x + 28} ${y}`}
+                  fill="none"
+                  className="tooltip-line"
+                />
+                <text x={x + 32} y={y} className="tooltip">
+                  {d.students}
+                </text>
+              </g>
+            );
+          })()}
       </g>
     </svg>
   );
